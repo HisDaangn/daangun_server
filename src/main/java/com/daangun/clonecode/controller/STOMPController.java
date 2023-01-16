78bf8205c942d704ce07cbf3d84fe04359fa7349
lukehongg <lukehongg@handong.ac.kr>
2023-01-16 20:33:42 +0900

    [#35]: 채팅 메세지 API 구현

--- src/main/java/com/daangun/clonecode/controller/STOMPController.java
+++ src/main/java/com/daangun/clonecode/controller/STOMPController.java
@@ -6,10 +6,15 @@ import com.daangun.clonecode.model.User;
 import com.daangun.clonecode.service.ChatMessageService;
 import com.daangun.clonecode.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,19 +28,25 @@ public class STOMPController {
     //Client가 SEND할 수 있는 경로
     //stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
     //"/pub/chat/enter"
-    @MessageMapping(value = "/chat/enter")
-    public void enter(@RequestBody ChatMessageRequest message){
-        User user = userService.findById(message.getWriterId());
-        message.setMessage(user.getName() + "님이 채팅방에 참여하였습니다.");
-        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
-    }
+//    @MessageMapping(value = "/chat/enter")
+//    public void enter(@RequestBody ChatMessageRequest message){
+//        User user = userService.findById(message.getWriterId());
+//        message.setMessage(user.getName() + "님이 채팅방에 참여하였습니다.");
+//        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
+//    }
 
-    @MessageMapping(value = "/chat/message")
-    public void message(@RequestBody ChatMessageRequest message){
+    @MessageMapping(value = "/chat/room/{roomId}")
+    public void message(@PathVariable String roomId, @RequestBody ChatMessageRequest message){
         chatMessageService.save(ChatMessage.from(message));
-        template.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
+        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 
+    // RoomId -> Message 전부 가져오는 API
+    @GetMapping(value="/chat/room/{roomId}")
+    public ResponseEntity<List<ChatMessage>> findAllByRoomId(@PathVariable String roomId){
+        List<ChatMessage> response = chatMessageService.findChatMessageByRoomId(roomId);
+        return ResponseEntity.ok(response);
+    }
 
 
 }
--- src/main/java/com/daangun/clonecode/service/ChatMessageServiceImpl.java
+++ src/main/java/com/daangun/clonecode/service/ChatMessageServiceImpl.java
@@ -6,14 +6,20 @@ import com.daangun.clonecode.repository.ChatMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 public class ChatMessageServiceImpl implements ChatMessageService {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
+    @Autowired
     private UserService userService;
+
+    @Autowired
     private ChatRoomService chatRoomService;
+
 
 
     public ChatMessage save(ChatMessage message) {
@@ -22,6 +28,9 @@ public class ChatMessageServiceImpl implements ChatMessageService {
         return ret;
     }
 
-
-
+    @Override
+    public List<ChatMessage> findChatMessageByRoomId(String roomId) {
+        List<ChatMessage> response = chatMessageRepository.findChatMessageByRoomId(roomId);
+        return response;
+    }
 }
